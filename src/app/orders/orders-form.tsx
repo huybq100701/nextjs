7924137f04030142fa254439f54a097919e41bd5
huybq_107 <64139999+huybq100701@users.noreply.github.com>
2023-10-04 10:27:22 +0700

    finished orderdata

--- src/app/orders/orders-form.tsx
+++ src/app/orders/orders-form.tsx
@@ -1,20 +1,14 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import { getOrders } from '../api/orders/orders';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'; // Import the CSS styles
+import moment from 'moment';
 
-interface Order {
-  id: number;
-  code: string;
-  channel: string;
-  total_price: number;
-  order_date: string;
-  order_status: number;
-  email: string;
-  full_name: string;
-}
+const localizer = momentLocalizer(moment);
 
 const OrdersForm = () => {
-  const [events, setEvents] = useState<Order[]>([]);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,7 +22,11 @@ const OrdersForm = () => {
 
         const response = await getOrders(payload);
         if (response && response.result && Array.isArray(response.result)) {
-          setEvents(response.result);
+          setEvents(response.result.map((event) => ({
+            title: event.code,
+            start: new Date(event.order_date),
+            end: new Date(event.order_date),
+          })));
           console.log('Fetched orders data:', response.result);
         } else {
           console.error('Invalid response data:', response);
@@ -45,17 +43,21 @@ const OrdersForm = () => {
     <div className="container mx-auto">
       <section className="order-data bg-gray-100 p-4">
         <h2 className="text-2xl font-bold">Orders Data</h2>
+        <div className="mt-4">
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: '500px' }} 
+          />
+        </div>
         <ul className="mt-4">
-          {events.map((event) => (
-            <li key={event.id} className="border-b py-2">
+          {events.map((event, index) => (
+            <li key={index} className="border-b py-2">
               <p className="text-lg font-semibold">ID: {event.id}</p>
-              <p>Code: {event.code}</p>
-              <p>Channel: {event.channel}</p>
-              <p>Total Price: {event.total_price}</p>
-              <p>Order Date: {event.order_date}</p>
-              <p>Order Status: {event.order_status}</p>
-              <p>Email: {event.email}</p>
-              <p>Full Name: {event.full_name}</p>
+              <p>Code: {event.title}</p>
+              <p>Order Date: {moment(event.start).format('YYYY-MM-DD HH:mm')}</p>
             </li>
           ))}
         </ul>
